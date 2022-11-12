8a09650dec33ccb3aca23ac04641da0997b6074f
talkkonnect <suvir@talkkonnect.com>
2022-11-12 23:31:39 +0700

    taking shape

--- barrier/barrier.c
+++ barrier/barrier.c
@@ -2,8 +2,8 @@
  * Copyright (c) 2022 Dynamic IT Solutions CO., LTD
  * Serial Traffic Barrier and Traffic Light Controller
  * Suvir Kumar <suvir@dits.co.th>
- * version 0.1 updates 10/11/2022 at 12:57
- * Raspbery Pi Pico
+ * version 0.2 updates 12/11/2022 at 22:49
+ * Raspbery Pi Pico with ETT GOIO Board
  */
 
 #include "hardware/uart.h"
@@ -12,34 +12,34 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LOW 0
+#define LOW  0
 #define HIGH 1
 
-const uint LED_PIN = 25;    // On Board LED (Output)
+const char VERSION[] = "Version 0.3 Released 12/11/2022";
+
+const uint LED_PIN    = 25; // Pico On Board LED (Output LED)
 const uint ETT_PORT_0 = 16; // Barrier Up (Output Relay)
 const uint ETT_PORT_1 = 17; // Barrier Down (Output Relay)
 const uint ETT_PORT_2 = 18; // Push up, Push Down (Output Relay)
 const uint ETT_PORT_3 = 19; // Barrier Position UP (Input Opto)
 const uint ETT_PORT_4 = 20; // Barrier Position Down (Input Opto)
-const uint ETT_PORT_5 = 21; // Relay Red Light 220V (Output)
-const uint ETT_PORT_6 = 22; // Relay Green Light 220V (Output)
-const uint ETT_PORT_7 = 28; // Beeper
+const uint ETT_PORT_5 = 21; // Red Light 220V (Output Relay)
+const uint ETT_PORT_6 = 22; // Green Light 220V (Output Relay)
+const uint ETT_PORT_7 = 28; // Beeper (Output Beeper)
 
+const uint BARRIER_UP_CMD       = ETT_PORT_0;
+const uint BARRIER_DOWN_CMD     = ETT_PORT_1;
+const uint BARRIER_PUSH_BTN_CMD = ETT_PORT_2;
+const uint BARRIER_UP_SIGNAL    = ETT_PORT_3;
+const uint BARRIER_DOWN_SIGNAL  = ETT_PORT_4;
+const uint TRAFFIC_LIGHT_RED    = ETT_PORT_5;
+const uint TRAFFIC_LIGHT_GREEN  = ETT_PORT_6;
+const uint BEEPER               = ETT_PORT_7;
 
-const uint BarrierUpCmd = ETT_PORT_0;
-const uint BarrierDownCmd = ETT_PORT_1;
-const uint BarrierPushBtnCmd = ETT_PORT_2;
-const uint BarrierUpSignal = ETT_PORT_3;
-const uint BarrierDownSignal = ETT_PORT_4;
-const uint TrafficLightRed = ETT_PORT_5;
-const uint TrafficLightGreen = ETT_PORT_6;
-const uint Beeper = ETT_PORT_7;
-
-void sendBarrierPulse(uint BarrierCmd, int delayBefore, int delayAfter) {
+void sendBarrierPulse(uint BarrierCmd, int delayBefore) {
   gpio_put(BarrierCmd, HIGH);
   sleep_ms(delayBefore);
   gpio_put(BarrierCmd, LOW);
-  sleep_ms(delayAfter);
   return;
 }
 
@@ -56,7 +56,8 @@ void sendTrafficSignalOff(uint TrafficCmd) {
 void showMenu(void) {
   printf("\rDynamic IT Solutions Parking Barrier Controller\n");
   printf("\rBy Suvir Kumar <suvir@dits.co.th>\n");
-  printf("\r\nMain Menu.\n");
+  printf("%s\n", VERSION);
+  printf("\r\nMain Menu\n");
   printf("\n\rc Chk Comm\n\r? Clear Screen & Show Menu\n\r1 Toggle Onboard "
          "LED\n\r2 Barrier Up\n\r3 Barrier Down\n\r4 Barrier Toggle\n\r5 "
          "Barrier Position\n\r6 Red Light On\n\r7 Red Light Off\n\r8 Green "
@@ -67,107 +68,123 @@ void showMenu(void) {
 int main() {
   int onboard_led = 0;
   int beeper_state = 0;
+  int barrierdownsignal = 0;
+  int barrierupsignal = 0;
 
   gpio_init(LED_PIN);
-  gpio_init(BarrierUpCmd);
-  gpio_init(BarrierDownCmd);
-  gpio_init(BarrierPushBtnCmd);
-  gpio_init(BarrierUpSignal);
-  gpio_init(BarrierDownSignal);
-  gpio_init(TrafficLightRed);
-  gpio_init(TrafficLightGreen);
-  gpio_init(Beeper);
+  gpio_init(BARRIER_UP_CMD);
+  gpio_init(BARRIER_DOWN_CMD);
+  gpio_init(BARRIER_PUSH_BTN_CMD);
+  gpio_init(BARRIER_UP_SIGNAL);
+  gpio_init(BARRIER_DOWN_SIGNAL);
+  gpio_init(TRAFFIC_LIGHT_RED);
+  gpio_init(TRAFFIC_LIGHT_GREEN);
+  gpio_init(BEEPER);
 
   gpio_set_dir(LED_PIN, GPIO_OUT);
 
-  gpio_set_dir(BarrierUpCmd, GPIO_OUT);
+  gpio_set_dir(BARRIER_UP_CMD, GPIO_OUT);
 
-  gpio_set_dir(BarrierDownCmd, GPIO_OUT);
+  gpio_set_dir(BARRIER_DOWN_CMD, GPIO_OUT);
 
-  gpio_set_dir(BarrierPushBtnCmd, GPIO_OUT);
+  gpio_set_dir(BARRIER_PUSH_BTN_CMD, GPIO_OUT);
 
-  gpio_set_dir(BarrierUpSignal, GPIO_IN);
-  gpio_pull_up(BarrierUpSignal);
+  gpio_set_dir(BARRIER_UP_SIGNAL, GPIO_IN);
+  gpio_pull_up(BARRIER_UP_SIGNAL);
 
-  gpio_set_dir(BarrierDownSignal, GPIO_IN);
-  gpio_pull_up(BarrierDownSignal);
+  gpio_set_dir(BARRIER_DOWN_SIGNAL, GPIO_IN);
+  gpio_pull_up(BARRIER_DOWN_SIGNAL);
 
-  gpio_set_dir(TrafficLightRed, GPIO_OUT);
+  gpio_set_dir(TRAFFIC_LIGHT_RED, GPIO_OUT);
 
-  gpio_set_dir(TrafficLightGreen, GPIO_OUT);
+  gpio_set_dir(TRAFFIC_LIGHT_GREEN, GPIO_OUT);
 
-  gpio_set_dir(Beeper, GPIO_OUT);
+  gpio_set_dir(BEEPER, GPIO_OUT);
 
-  char buffer[1024];
+  //char buffer[1024];
+  char buffer;
 
   stdio_init_all();
   showMenu();
 
   while (true) {
-    scanf("%1024s", buffer);
+    buffer = getchar();
+    //scanf("%1024s", buffer);
 
-    if (strcmp(buffer, "c") == 0) {
-      printf(":COMM OK$\n");
+    if (buffer == 'c') {
+      printf(":COMM$\n");
     }
-    if (strcmp(buffer, "?") == 0) {
+    if (buffer == '?') {
       printf("%c%c%c%c", 0x1B, 0x5B, 0x32, 0x4A);
       showMenu();
     }
-    if (strcmp(buffer, "1") == 0) {
+    if (buffer == '1') {
       if (onboard_led == 0) {
         gpio_put(LED_PIN, HIGH);
         onboard_led = 1;
+      printf(":OB LED ON$\n");
       } else if (onboard_led == 1) {
         gpio_put(LED_PIN, LOW);
         onboard_led = 0;
+      printf(":OB LED OFF$\n");
       }
     }
-    if (strcmp(buffer, "2") == 0) {
-      sendBarrierPulse(BarrierUpCmd, 1000, 2000);
+    if (buffer == '2') {
+      sendBarrierPulse(BARRIER_UP_CMD, 1000);
+      printf(":BR UP$\n");
     }
-    if (strcmp(buffer, "3") == 0) {
-      sendBarrierPulse(BarrierDownCmd, 1000, 2000);
+    if (buffer == '3') {
+      sendBarrierPulse(BARRIER_DOWN_CMD, 1000);
+      printf(":BR DOWN$\n");
     }
-    if (strcmp(buffer, "4") == 0) {
-      sendBarrierPulse(BarrierPushBtnCmd, 1000, 2000);
+    if (buffer == '4') {
+      sendBarrierPulse(BARRIER_PUSH_BTN_CMD, 1000);
+      printf(":BR PUSHBUTTON$\n");
     }
-    if (strcmp(buffer, "5") == 0) {
-      if (gpio_get(BarrierUpSignal) == 0 && gpio_get(BarrierDownSignal) == 0) {
-        printf("Barrier Undetermined State!\n\n");
+    if (buffer == '5') {
+      if (gpio_get(BARRIER_UP_SIGNAL) == 0 && gpio_get(BARRIER_DOWN_SIGNAL) == 0) {
+        printf(":BRPOS UNDERTERMINED\n");
       }
-      if (gpio_get(BarrierUpSignal) == 0 && gpio_get(BarrierDownSignal) == 1) {
-        printf("Barrier is Down!\n\n");
+      if (gpio_get(BARRIER_UP_SIGNAL) == 0 && gpio_get(BARRIER_DOWN_SIGNAL) == 1) {
+        printf(":BRPOS DOWN$\n");
       }
-      if (gpio_get(BarrierUpSignal) == 1 && gpio_get(BarrierDownSignal) == 0) {
-        printf("Barrier is Up!\n\n");
+      if (gpio_get(BARRIER_UP_SIGNAL) == 1 && gpio_get(BARRIER_DOWN_SIGNAL) == 0) {
+        printf(":BRPOS UP$\n");
       }
-      if (gpio_get(BarrierUpSignal) == 1 && gpio_get(BarrierDownSignal) == 1) {
-        printf("Barrier Undetermined State!\n\n");
+      if (gpio_get(BARRIER_UP_SIGNAL) == 1 && gpio_get(BARRIER_DOWN_SIGNAL) == 1) {
+        printf(":BRPOS UNDERTERMINED$\n");
       }
     }
-    if (strcmp(buffer, "6") == 0) {
-      sendTrafficSignalOn(TrafficLightRed);
+    if (buffer == '6') {
+      sendTrafficSignalOn(TRAFFIC_LIGHT_RED);
+        printf(":TLRED ON$\n");
     }
-    if (strcmp(buffer, "7") == 0) {
-      sendTrafficSignalOff(TrafficLightRed);
+    if (buffer == '7') {
+      sendTrafficSignalOff(TRAFFIC_LIGHT_RED);
+        printf(":TLRED OFF$\n");
     }
-    if (strcmp(buffer, "8") == 0) {
-      sendTrafficSignalOn(TrafficLightGreen);
+    if (buffer == '8') {
+      sendTrafficSignalOn(TRAFFIC_LIGHT_GREEN);
+        printf(":TLGRN ON$\n");
     }
-    if (strcmp(buffer, "9") == 0) {
-      sendTrafficSignalOff(TrafficLightGreen);
+    if (buffer == '9') {
+      sendTrafficSignalOff(TRAFFIC_LIGHT_GREEN);
+        printf(":TLGRN OFF$\n");
     }
-    if (strcmp(buffer, "0") == 0) {
+    if (buffer == '0') {
       if (beeper_state == 0) {
-        gpio_put(Beeper, HIGH);
+        gpio_put(BEEPER, HIGH);
         beeper_state = 1;
+        printf(":BP ON$\n");
       } else if (beeper_state == 1) {
-        gpio_put(Beeper, LOW);
+        gpio_put(BEEPER, LOW);
         beeper_state = 0;
+        printf(":BP OFF$\n");
       }
     }
 
-    if (strcmp(buffer, "PROGRAM") == 0) {
+    if (buffer == 'P') {
+        printf(":UPLOAD FIRMWARE MODE$\n");
       reset_usb_boot(0, 0);
     }
   }
