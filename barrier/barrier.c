b8c1663fdcf8206c6be70f0164f17aaab9925748
talkkonnect <suvir@talkkonnect.com>
2022-11-13 22:21:48 +0700

    first stable working release

--- barrier/barrier.c
+++ barrier/barrier.c
@@ -2,7 +2,6 @@
  * Copyright (c) 2022 Dynamic IT Solutions CO., LTD
  * Serial Traffic Barrier and Traffic Light Controller
  * Suvir Kumar <suvir@dits.co.th>
- * version 0.2 updates 12/11/2022 at 22:49
  * Raspbery Pi Pico with ETT GOIO Board
  */
 
@@ -12,14 +11,26 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LOW 0
-#define HIGH 1
-
+#define UART0_ID uart0
+#define BAUD0_RATE 115200
+#define DATA0_BITS 8
+#define STOP0_BITS 1
+#define PARITY0    UART_PARITY_NONE
 #define UART0_TX_PIN 0
 #define UART0_RX_PIN 1
 
+#define UART1_ID uart1
+#define BAUD1_RATE 115200
+#define DATA1_BITS 8
+#define STOP1_BITS 1
+#define PARITY1    UART_PARITY_NONE
+#define UART1_TX_PIN 8
+#define UART1_RX_PIN 9
 
-const char VERSION[] = "Version 0.4 Released 13/11/2022";
+#define LOW 0
+#define HIGH 1
+
+const char VERSION[] = "Version 1.0 Released 13/11/2022 at 22:20 (ICT)\r\n";
 
 const uint LED_PIN = 25;    // Pico On Board LED (Output LED)
 const uint ETT_PORT_0 = 16; // Barrier Up (Output Relay)
@@ -40,6 +51,11 @@ const uint TRAFFIC_LIGHT_RED = ETT_PORT_5;
 const uint TRAFFIC_LIGHT_GREEN = ETT_PORT_6;
 const uint BEEPER = ETT_PORT_7;
 
+int onboard_led = 0;
+int beeper_state = 0;
+int current_barrier_state = 2;
+int previous_barrier_state = 2;
+
 void sendBarrierPulse(uint BarrierCmd, int delayBefore) {
   gpio_put(BarrierCmd, HIGH);
   sleep_ms(delayBefore);
@@ -58,22 +74,153 @@ void sendTrafficSignalOff(uint TrafficCmd) {
 }
 
 void showMenu(void) {
-  printf("\rDynamic IT Solutions Parking Barrier Controller\n");
-  printf("\rBy Suvir Kumar <suvir@dits.co.th>\n");
-  printf("%s\n", VERSION);
-  printf("\r\nMain Menu\n");
-  printf("\n\rc Chk Comm\n\r? Clear Screen & Show Menu\n\r1 Toggle Onboard "
-         "LED\n\r2 Barrier Up\n\r3 Barrier Down\n\r4 Barrier Toggle\n\r5 "
-         "Barrier Position\n\r6 Red Light On\n\r7 Red Light Off\n\r8 Green "
-         "Light On\n\r9 Green Light Off\n\r0 Beep\n\r\n");
+  uart_puts(UART0_ID, "Dynamic IT Solutions Parking Barrier Controller\r\n");
+  uart_puts(UART0_ID, "By Suvir Kumar <suvir@dits.co.th>\r\n");
+  uart_puts(UART0_ID, VERSION);
+  uart_puts(UART0_ID,"\r\nMain Menu\r\n");
+  uart_puts(UART0_ID,"\r\nc Chk Comm\r\n? Clear Screen & Show Menu\r\n1 Toggle Onboard "
+         "LED\r\n2 Barrier Up\r\n3 Barrier Down\r\n4 Barrier Toggle\r\n5 "
+         "Barrier Position\r\n6 Red Light On\r\n7 Red Light Off\r\n8 Green "
+         "Light On\r\n9 Green Light Off\r\n0 Beep\r\n");
+
+  uart_puts(UART1_ID, "Dynamic IT Solutions Parking Barrier Controller\r\n");
+  uart_puts(UART1_ID, "By Suvir Kumar <suvir@dits.co.th>\r\n");
+  uart_puts(UART1_ID, VERSION);
+  uart_puts(UART1_ID,"\r\nMain Menu\r\n");
+  uart_puts(UART1_ID,"\r\nc Chk Comm\r\n? Clear Screen & Show Menu\r\n1 Toggle Onboard "
+         "LED\r\n2 Barrier Up\r\n3 Barrier Down\r\n4 Barrier Toggle\r\n5 "
+         "Barrier Position\r\n6 Red Light On\r\n7 Red Light Off\r\n8 Green "
+         "Light On\r\n9 Green Light Off\r\n0 Beep\r\n");
+  
   return;
 }
 
+void respond(char buffer)  {
+      if (buffer == 'c') {
+        uart_puts(UART0_ID,"'{\"ack\":\"COM\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"COM\"}'\r\n");
+      }
+      if (buffer == '?') {
+        uart_putc(UART0_ID, 0x1B);
+        uart_putc(UART0_ID, 0x5B);
+        uart_putc(UART0_ID, 0x32);
+        uart_putc(UART0_ID, 0x4A);
+        uart_putc(UART1_ID, 0x1B);
+        uart_putc(UART1_ID, 0x5B);
+        uart_putc(UART1_ID, 0x32);
+        uart_putc(UART1_ID, 0x4A);
+        showMenu();
+      }
+      if (buffer == '1') {
+        if (onboard_led == 0) {
+          gpio_put(LED_PIN, HIGH);
+          onboard_led = 1;
+          uart_puts(UART0_ID,"'{\"ack\":\"OB LED ON\"}'\r\n");
+          uart_puts(UART1_ID,"'{\"ack\":\"OB LED ON\"}'\r\n");
+        } else if (onboard_led == 1) {
+          gpio_put(LED_PIN, LOW);
+          onboard_led = 0;
+          uart_puts(UART0_ID,"'{\"ack\":\"OB LED OFF\"}'\r\n");
+          uart_puts(UART1_ID,"'{\"ack\":\"OB LED OFF\"}'\r\n");
+        }
+      }
+      if (buffer == '2') {
+        sendBarrierPulse(BARRIER_UP_CMD, 1000);
+        uart_puts(UART0_ID,"'{\"ack\":\"BR UP\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"BR UP\"}'\r\n");
+      }
+      if (buffer == '3') {
+        sendBarrierPulse(BARRIER_DOWN_CMD, 1000);
+        uart_puts(UART0_ID,"'{\"ack\":\"BR DWN\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"BR DWN\"}'\r\n");
+      }
+      if (buffer == '4') {
+        sendBarrierPulse(BARRIER_PUSH_BTN_CMD, 1000);
+        uart_puts(UART0_ID,"'{\"ack\":\"BR PB\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"BR PB\"}'\r\n");
+      }
+      if (buffer == '5') {
+        if (gpio_get(BARRIER_UP_SIGNAL) == 0 &&
+            gpio_get(BARRIER_DOWN_SIGNAL) == 0) {
+            uart_puts(UART0_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART0_ID,"'{\"state\":\"BR UNDEF\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"state\":\"BR UNDEF\"}'\r\n");
+        }
+        if (gpio_get(BARRIER_UP_SIGNAL) == 0 &&
+            gpio_get(BARRIER_DOWN_SIGNAL) == 1) {
+            uart_puts(UART0_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART0_ID,"'{\"state\":\"BR DWN\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"state\":\"BR DWN\"}'\r\n");
+        }
+        if (gpio_get(BARRIER_UP_SIGNAL) == 1 &&
+            gpio_get(BARRIER_DOWN_SIGNAL) == 0) {
+            uart_puts(UART0_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART0_ID,"'{\"state\":\"BR UP\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"state\":\"BR UP\"}'\r\n");
+        }
+        if (gpio_get(BARRIER_UP_SIGNAL) == 1 &&
+            gpio_get(BARRIER_DOWN_SIGNAL) == 1) {
+            uart_puts(UART0_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART0_ID,"'{\"state\":\"BR UNDEF\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"ack\":\"BR POS\"}'\r\n");
+            uart_puts(UART1_ID,"'{\"state\":\"BR UNDEF\"}'\r\n");
+        }
+      }
+      if (buffer == '6') {
+        sendTrafficSignalOn(TRAFFIC_LIGHT_RED);
+        uart_puts(UART0_ID,"'{\"ack\":\"TL RED ON\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"TL RED ON\"}'\r\n");
+      }
+      if (buffer == '7') {
+        sendTrafficSignalOff(TRAFFIC_LIGHT_RED);
+        uart_puts(UART0_ID,"'{\"ack\":\"TL RED OFF\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"TL RED OFF\"}'\r\n");
+      }
+      if (buffer == '8') {
+        sendTrafficSignalOn(TRAFFIC_LIGHT_GREEN);
+        uart_puts(UART0_ID,"'{\"ack\":\"TL GRN ON\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"TL GRN ON\"}'\r\n");
+      }
+      if (buffer == '9') {
+        sendTrafficSignalOff(TRAFFIC_LIGHT_GREEN);
+        uart_puts(UART0_ID,"'{\"ack\":\"TL GRN OFF\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"ack\":\"TL GRN OFF\"}'\r\n");
+      }
+      if (buffer == '0') {
+        if (beeper_state == 0) {
+          gpio_put(BEEPER, HIGH);
+          beeper_state = 1;
+         uart_puts(UART0_ID,"'{\"ack\":\"BEEP ON\"}'\r\n");
+         uart_puts(UART1_ID,"'{\"ack\":\"BEEP ON\"}'\r\n");
+        } else if (beeper_state == 1) {
+          gpio_put(BEEPER, LOW);
+          beeper_state = 0;
+         uart_puts(UART0_ID,"'{\"ack\":\"BEEP OFF\"}'\r\n");
+         uart_puts(UART1_ID,"'{\"ack\":\"BEEP OFF\"}'\r\n");
+        }
+      }
+      if (buffer == 'U') {
+         uart_puts(UART0_ID,"'{\"ack\":\"FIRMWARE UPLOAD\"}'\r\n");
+         uart_puts(UART1_ID,"'{\"ack\":\"FIRMWARE UPLOAD\"}'\r\n");
+        reset_usb_boot(0, 0);
+      }
+}
+
 int main() {
-  int onboard_led = 0;
-  int beeper_state = 0;
-  int current_barrier_state = 2;
-  int previous_barrier_state = 2;
+  uart_init(UART0_ID, BAUD0_RATE);
+  gpio_set_function(UART0_TX_PIN, UART0_TX_PIN);
+  gpio_set_function(UART0_RX_PIN, UART0_RX_PIN);
+  uart_set_hw_flow(UART0_ID, false, false);
+  uart_set_format(UART0_ID, DATA0_BITS, STOP0_BITS, PARITY0);
+  
+  uart_init(UART1_ID, BAUD1_RATE);
+  gpio_set_function(UART1_TX_PIN, UART1_TX_PIN);
+  gpio_set_function(UART1_RX_PIN, UART1_RX_PIN);
+  uart_set_hw_flow(UART1_ID, false, false);
+  uart_set_format(UART1_ID, DATA1_BITS, STOP1_BITS, PARITY1);
 
   gpio_init(LED_PIN);
   gpio_init(BARRIER_UP_CMD);
@@ -86,29 +233,26 @@ int main() {
   gpio_init(BEEPER);
 
   gpio_set_dir(LED_PIN, GPIO_OUT);
-
   gpio_set_dir(BARRIER_UP_CMD, GPIO_OUT);
-
   gpio_set_dir(BARRIER_DOWN_CMD, GPIO_OUT);
-
   gpio_set_dir(BARRIER_PUSH_BTN_CMD, GPIO_OUT);
-
   gpio_set_dir(BARRIER_UP_SIGNAL, GPIO_IN);
   gpio_pull_up(BARRIER_UP_SIGNAL);
-
   gpio_set_dir(BARRIER_DOWN_SIGNAL, GPIO_IN);
   gpio_pull_up(BARRIER_DOWN_SIGNAL);
-
   gpio_set_dir(TRAFFIC_LIGHT_RED, GPIO_OUT);
-
   gpio_set_dir(TRAFFIC_LIGHT_GREEN, GPIO_OUT);
-
   gpio_set_dir(BEEPER, GPIO_OUT);
+  gpio_set_function(0, GPIO_FUNC_UART);
+  gpio_set_function(1, GPIO_FUNC_UART);
+  gpio_set_function(8, GPIO_FUNC_UART);
+  gpio_set_function(9, GPIO_FUNC_UART);
 
-  // char buffer[1024];
+
   char buffer;
+  char buffer1;
 
-  stdio_init_all();
+  //stdio_init_all();
   showMenu();
 
   while (true) {
@@ -132,103 +276,29 @@ int main() {
 
     if (current_barrier_state != previous_barrier_state) {
       if (current_barrier_state == 0) {
-        printf("'{\"state\":\"BR DWN\"}'\n");
+        uart_puts(UART0_ID,"'{\"state\":\"BR DWN\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"state\":\"BR DWN\"}'\r\n");
       }
       if (current_barrier_state == 1) {
-        printf("'{\"state\":\"BR UP\"}'\n");
+        uart_puts(UART0_ID,"'{\"state\":\"BR UP\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"state\":\"BR UP\"}'\r\n");
       }
       if (current_barrier_state == 2) {
-        printf("'{\"state\":\"BR UNDEF\"}'\n");
+        uart_puts(UART0_ID,"'{\"state\":\"BR UNDEF\"}'\r\n");
+        uart_puts(UART1_ID,"'{\"state\":\"BR UNDEF\"}'\r\n");
       }
       previous_barrier_state = current_barrier_state;
     }
 
-    while (uart_is_readable(uart0)) {
-      buffer = getchar();
-
-      if (buffer == 'c') {
-        printf("'{\"ack\":\"COM\"}'\n");
-      }
-      if (buffer == '?') {
-        printf("%c%c%c%c", 0x1B, 0x5B, 0x32, 0x4A);
-        showMenu();
-      }
-      if (buffer == '1') {
-        if (onboard_led == 0) {
-          gpio_put(LED_PIN, HIGH);
-          onboard_led = 1;
-          printf("'{\"ack\":\"OB LED ON\"}'\n");
-        } else if (onboard_led == 1) {
-          gpio_put(LED_PIN, LOW);
-          onboard_led = 0;
-          printf("'{\"ack\":\"OB LED OFF\"}'\n");
-        }
-      }
-      if (buffer == '2') {
-        sendBarrierPulse(BARRIER_UP_CMD, 1000);
-        printf("'{\"ack\":\"BR UP\"}'\n");
-      }
-      if (buffer == '3') {
-        sendBarrierPulse(BARRIER_DOWN_CMD, 1000);
-        printf("'{\"ack\":\"BR DWN\"}'\n");
-      }
-      if (buffer == '4') {
-        sendBarrierPulse(BARRIER_PUSH_BTN_CMD, 1000);
-        printf("'{\"ack\":\"BR PB\"}'\n");
-      }
-      if (buffer == '5') {
-        if (gpio_get(BARRIER_UP_SIGNAL) == 0 &&
-            gpio_get(BARRIER_DOWN_SIGNAL) == 0) {
-            printf("'{\"ack\":\"BR POS\"}'\n");
-            printf("'{\"state\":\"BR UNDEF\"}'\n");
-        }
-        if (gpio_get(BARRIER_UP_SIGNAL) == 0 &&
-            gpio_get(BARRIER_DOWN_SIGNAL) == 1) {
-            printf("'{\"ack\":\"BR POS\"}'\n");
-            printf("'{\"state\":\"BR DWN\"}'\n");
-        }
-        if (gpio_get(BARRIER_UP_SIGNAL) == 1 &&
-            gpio_get(BARRIER_DOWN_SIGNAL) == 0) {
-            printf("'{\"ack\":\"BR POS\"}'\n");
-            printf("'{\"state\":\"BR UP\"}'\n");
-        }
-        if (gpio_get(BARRIER_UP_SIGNAL) == 1 &&
-            gpio_get(BARRIER_DOWN_SIGNAL) == 1) {
-            printf("'{\"ack\":\"BR POS\"}'\n");
-            printf("'{\"state\":\"BR UNDEF\"}'\n");
-        }
-      }
-      if (buffer == '6') {
-        sendTrafficSignalOn(TRAFFIC_LIGHT_RED);
-        printf("'{\"ack\":\"TL RED ON\"}'\n");
-      }
-      if (buffer == '7') {
-        sendTrafficSignalOff(TRAFFIC_LIGHT_RED);
-        printf("'{\"ack\":\"TL RED OFF\"}'\n");
-      }
-      if (buffer == '8') {
-        sendTrafficSignalOn(TRAFFIC_LIGHT_GREEN);
-        printf("'{\"ack\":\"TL GRN ON\"}'\n");
-      }
-      if (buffer == '9') {
-        sendTrafficSignalOff(TRAFFIC_LIGHT_GREEN);
-        printf("'{\"ack\":\"TL GRN OFF\"}'\n");
-      }
-      if (buffer == '0') {
-        if (beeper_state == 0) {
-          gpio_put(BEEPER, HIGH);
-          beeper_state = 1;
-         printf("'{\"ack\":\"BEEP ON\"}'\n");
-        } else if (beeper_state == 1) {
-          gpio_put(BEEPER, LOW);
-          beeper_state = 0;
-         printf("'{\"ack\":\"BEEP OFF\"}'\n");
-        }
-      }
-      if (buffer == 'U') {
-         printf("'{\"ack\":\"FIRMWARE UPLOAD\"}'\n");
-        reset_usb_boot(0, 0);
-      }
+    while (uart_is_readable(uart0))  {
+      buffer = uart_getc(uart0);
+      respond(buffer);
     }
+    while (uart_is_readable(uart1))  {
+      buffer = uart_getc(uart1);
+      respond(buffer);
+    }
+
   }
-}
+  }
+
